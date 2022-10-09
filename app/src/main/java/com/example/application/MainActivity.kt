6d07b6c0944217d883cad0b8a0e37343c3fb1964
castro2123 <ana.castro@ipvc.pt>
2022-10-09 18:07:38 +0100

    Initial commit

---
+++ app/src/main/java/com/example/application/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.application
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("TAG", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("TAG", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("TAG", "onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("TAG", "onDestroy")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("TAG", "onStop")
+    }
+}
